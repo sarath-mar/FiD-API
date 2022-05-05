d5ba59404ad0e8f2bb6e05cee812b8acaebd80e0
LAPTOP-EHDI9A5M\sarat <sarath@armino.in>
2022-05-05 22:14:43 +0530

    helper class included

--- posts/resolver.js
+++ posts/resolver.js
@@ -1,35 +1,11 @@
 const { Post } = require("../models/Post");
+const PostHelper = require('./helper')
 
 module.exports.queries = {
-    getPosts: async () => {
-        try {
-            let posts = await Post.find()
-            console.log(posts)
-            posts.length ? console.log('there is post') : console.log("no posts");
-            return posts
-        } catch (e) {
-            console.log(e);
-            throw "Error"
-        }
-
-    }
+    getPosts: async () => PostHelper.getPosts()
 }
 module.exports.mutations = {
     createPost: async (_, { input }, context, info) => {
-        console.log("input")
-        console.log(input)
-        try {
-            if (input) {
-                let newPost = await Post.create(input)
-                if (newPost) {
-                    console.log(newPost)
-                    return newPost
-                }
-            } else {
-                throw new Error("No Input")
-            }
-        } catch (e) {
-            throw new Error(e)
-        }
+        return PostHelper.createPosts(input)
     }
 }
